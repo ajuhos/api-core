ff5744ff5f14674375d5e5b6d134a71336ae0c4f
Juhos Ádám <ajuhos@we-are-break.com>
2016-10-21 12:26:13 +0200

    Update package version

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+var ApiEdgeQueryType_1 = require("./src/ApiEdgeQueryType");
+exports.ApiEdgeQueryType = ApiEdgeQueryType_1.ApiEdgeQueryType;
+var ApiEdgeQuery_1 = require("./src/ApiEdgeQuery");
+exports.ApiEdgeQuery = ApiEdgeQuery_1.ApiEdgeQuery;
+//# sourceMappingURL=index.js.map
