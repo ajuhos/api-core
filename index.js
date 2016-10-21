1ef3ee4b43e6c743c4eb113df602f642af7f8304
Juhos Ádám <ajuhos@we-are-break.com>
2016-10-21 12:11:52 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.ApiEdgeDefinition = require("./src/ApiEdgeDefinition").ApiEdgeDefinition;
+exports.ApiEdgeQueryType = require("./src/ApiEdgeQueryType").ApiEdgeQueryType;
+exports.ApiEdgeQuery = require("./src/ApiEdgeQuery").ApiEdgeQuery;
